0a5c22e639e82656c8accd1c1fd30efe4aeb06e1
shawnycx <scy12a@acu.edu>
2016-09-19 10:10:51 -0500

    Setting up project folder and some files

---
+++ Courses/step_definitions/CourseTest.java
@@ -0,0 +1,23 @@
+package step_definitions;
+// import implementation.Requirement;
+
+import static org.junit.Assert.*;
+
+import cucumber.api.java.en.*;
+import cucumber.api.PendingException;
+
+public class Req_Steps {
+
+	@Given("^the class is \"([^\"]*)\"$")
+	public void theClassIs(String arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^the student \"([^\"]*)\" should not be in that class$")
+	public void theStudentShouldNotBeInThatClass(String arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+	
+}
