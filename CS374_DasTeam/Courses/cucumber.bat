549127683dbcdab4c84e5fe3bd74d3c66f8f36bd
shawn yap <scy12a@acu.edu>
2016-09-21 10:08:05 -0500

    Removed duplicate folder

--- CS374_DasTeam/Courses/cucumber.bat
+++
@@ -1,5 +0,0 @@
-javac -cp "jars/*;." step_definitions/CourseTest.java
-                     implementation/GetStudents.java \
-                     CSV_Parser/CSVParse.java
-
-java -cp "jars/*;." cucumber.api.cli.Main -p pretty --snippets camelcase -g step_definitions features
--- CS374_DasTeam/README.md
+++
@@ -1,9 +0,0 @@
-# CS374-DasTeam
-Team Name: Das Team
-
-#Team Members:
-1. Shawn Yap
-2. Ivan Anyaegbu
-3. Steven Qin
-
-This repository is created for the CS374 - Software engineering
