08d0276b53c683e2d2d5c12fdb945687aa5a9f0e
StevenQin1996 <sxq13a@acu.edu>
2016-09-20 23:02:51 -0500

    Add files via upload

---
+++ CS374_DasTeam/Courses/CSV_Parser/CSVParse.java
@@ -0,0 +1,37 @@
+package CSV_Parser;
+
+import com.opencsv.CSVReader;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Vector;
+ 
+public class CSVParse
+{
+    private Vector<String> studentNames = new Vector<String>(10,2);
+
+    public Vector<String> notInClass(String aCourse)
+    {
+        //Input file which needs to be parsed
+        String csvFile = "Something.csv";
+        CSVReader reader = null;
+        try
+        {
+            reader = new CSVReader(new FileReader(csvFile));
+            String[] line;
+            while ((line = reader.readNext()) != null) {
+                if (line[2] != aCourse)
+                {
+                    checkNameInVec(line[1]);
+                }
+            }
+        } 
+        catch (Exception e) {}
+        return studentNames;
+    }
+
+    private void checkNameInVec(String aName) {
+        if (!studentNames.contains(aName))
+            studentNames.add(aName);
+    }
+}
---
+++ CS374_DasTeam/Courses/step_definitions/CourseTest.java
@@ -0,0 +1,23 @@
+package step_definitions;
+import implementation.GetStudents;
+
+import static org.junit.Assert.*;
+
+import cucumber.api.java.en.*;
+import cucumber.api.PendingException;
+
+public class CourseTest {
+
+	GetStudents students = new GetStudents();
+
+	@Given("^the class is \"([^\"]*)\"$")
+	public void theClassIs(String courseName) throws Throwable {
+	    students.takeCourse(courseName);
+	}
+
+	@Then("^the student \"([^\"]*)\" should not be in that class$")
+	public void theStudentShouldNotBeInThatClass(String studentName) throws Throwable {
+	    assertEquals(studentName, students.notInClass());
+	}
+
+}
