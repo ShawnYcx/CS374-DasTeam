0bf840b65ffa651cb6577190d03c3026cafd00d3
STEVEN-PRIVATE\steven <SXQ13A@ACU.EDU>
2016-10-11 20:16:05 -0500

    changed test, preQ.csv Query.sql has the query to insert data into tables

--- CSV File/query.sql
+++ CSV File/query.sql
@@ -16,11 +16,23 @@ First_Name varchar(40),
 Last_Name varchar(40),
 Constraint student_course PRIMARY KEY  (`Pidm`,`Term_Code`,`Department_Code`,`CRN`,`Section_Number`,`Banner_ID`));
 
-
 LOAD DATA INFILE 'Test.csv' INTO TABLE cs374_anon
 FIELDS TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
---if not exists (select * from cs374_anon c where c.Pidm=@pdim and c.Term_Code=@Term_Code and c.Department_Code=@Department_Code and c.CRN=@CRN and c.Section_Number=@Section_Number and c.Banner_ID=@Banner_ID)
---INSERT INTO Delegates ([Pidm],[Term_Code],[Department_Code],[CRN],[Subject_Code],[Course_Number],[Section_Number],[Instructor],[Banner_ID],[First_Name],[Last_Name]) 
---values (@Pidm,@Term_Code,@Department_Code,@CRN,@Subject_Code,@Course_Number,@Section_Number,@Instructor,@Banner_ID,@First_Name,@Last_Name)
 IGNORE 1 LINES;
+
+DROP TABLE IF EXISTS course_Preq;
+CREATE TABLE course_Preq(
+Subject_Code varchar(10) NOT NULL, 
+Course_Number varchar(10) NOT NULL, 
+Course_Title varchar(100),
+Course_Preq_Title varchar(100),
+Constraint course_preq PRIMARY KEY(`Subject_Code`,`Course_Number`));
+
+LOAD DATA INFILE 'preQ.csv' INTO TABLE course_Preq
+FIELDS TERMINATED BY ',' ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n'
+IGNORE 1 LINES;
+
+select * from course_Preq limit 10;
+select * from cs374_anon limit 10;
