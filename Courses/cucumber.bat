2746405cb0657c50b23bf2b638755effec52003f
shawn yap <scy12a@acu.edu>
2016-10-14 10:31:34 -0500

    Cleaned up source folder

--- Courses/cucumber.bat
+++
@@ -1,3 +0,0 @@
-javac -cp "jars/*;." step_definitions/CourseTest.java implementation/getStudents.java 
-                   
-java -cp "jars/*;." cucumber.api.cli.Main -p pretty --snippets camelcase -g step_definitions features
